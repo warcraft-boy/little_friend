659e2d36bad393778da47a7f14b1187b193e012f
chenjianwen <ChenJevin@163.com>
2020-05-08 16:57:18 +0800

    1

--- spring_aop_demo/src/test/java/org/chen/SpringAopDemoApplicationTests.java
+++ spring_aop_demo/src/test/java/org/chen/SpringAopDemoApplicationTests.java
@@ -1,11 +1,15 @@
 package org.chen;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class SpringAopDemoApplicationTests {
+@SpringBootTest(classes = SpringAopDemoApplication.class)
+@RunWith(SpringRunner.class)
+public class SpringAopDemoApplicationTests {
 
+    
     @Test
     void contextLoads() {
     }
