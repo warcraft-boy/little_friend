0d9bc894075bcb100eda51be0eda202ab84eb883
chenjianwen <ChenJevin@163.com>
2020-05-08 16:58:27 +0800

    1

--- spring_aop_demo/src/test/java/org/chen/SpringAopDemoApplicationTests.java
+++ spring_aop_demo/src/test/java/org/chen/SpringAopDemoApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class SpringAopDemoApplicationTests {
 
-    
     @Test
     void contextLoads() {
     }
